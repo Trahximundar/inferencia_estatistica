1910484c483193b4bed9fa1b5b98186d757aa69f
Thraximundar <evertonbarbosapeixoto@gmail.com>
2024-04-29 20:31:10 -0300

    Atualização

--- 02_leitura_dados_mapa_temperatura.R
+++ 02_leitura_dados_mapa_temperatura.R
@@ -7,8 +7,6 @@ relevo <- raster('mapa_temperatura/dados/relevo_minas_gerais.tif') #Relevo
 
 mg <- read_state(code_state = "MG") #Limite Territorial de MG
 
-
-
 #Convertendo dados espaciais para data.frame
 top.df <- as.data.frame(relevo,xy=TRUE)
 top.df <- na.omit(top.df)
---
+++ 05_codigo.R
@@ -0,0 +1,22 @@
+#########################################
+#####   Plotando o mapa            ######
+#########################################
+ggplot(data=temp.mg)+ #Informando os dados de temperatura
+        geom_raster(aes(long,lat,fill=temp.cat))+#Definindo colunas de dados e plotando a temperatura
+        geom_sf(data = mg,fill='NA')+ #Sobrepondo o limite territorial de Minas Gerais
+        scale_fill_manual(values = tim.colors(9))+ #Alterando as cores
+        annotation_scale()+ #Inserindo escala no Mapa
+        annotation_north_arrow(location="tl", #Inserindo a orientação do mapa 
+                               style = north_arrow_fancy_orienteering())+ #Estabelecendo o estilo da orientação
+        labs(title = 'Temperatura Média Anual', #Definindo o Título do mapa
+             fill='[ºC]',x=NULL,y=NULL)+ #Definindo os Títulos dos eixos e das legendas
+        theme_light() #Definindo o tema
+
+
+#########################################
+#####   Salvando o mapa            ######
+#########################################
+ggsave(filename = 'figuras/mapa_temperatura.png',#nome do arquivo
+       width = 12,#Largura 
+       height = 10,#Altura
+       units = "cm")#Unidade
